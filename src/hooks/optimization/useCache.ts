98323b55e3dd7da4147ac67c90d3967e4ec66d88
David <77506410+gdavidev@users.noreply.github.com>
2025-01-22 00:53:21 -0300

    feat: configuration files added and react.util subtree included

--- src/hooks/optimization/useCache.ts
+++ src/hooks/optimization/useCache.ts
@@ -6,6 +6,14 @@ type UseCacheResult = {
     setCachedItem: (key: string, value: any) => void
 }
 
+/**
+ * @description Exposes an easy API to the CacheProvider context which will manage client-side in memory cache data. 
+ * @throws Error When not called inside the CacheProvider context element.
+ * 
+ * @returns {UseCacheResult} object:
+ * - `getCachedItem`: gets the data by its key or undefined;
+ * - `setCachedItem`: sets the data with a key, if it already exists, it will be overwritten.
+ */
 export default function useCache(): UseCacheResult {
     const cacheContext = useContext(CacheContext);
     if (!cacheContext) {
