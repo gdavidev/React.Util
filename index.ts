a8a4a0cca732fd29d78cbd9ebc91d3804ab104bb
David <77506410+gdavidev@users.noreply.github.com>
2025-02-13 21:39:55 -0300

    feat: all utilities exported on index.ts file

---
+++ index.ts
@@ -0,0 +1,15 @@
+// Optimization
+export * from './src/hooks/optimization/useCache';
+export * from './src/hooks/optimization/useDebounce';
+export * from './src/hooks/optimization/useThrottle';
+
+// Environment
+export * from './src/hooks/environment/useDeviceWidth';
+export * from './src/hooks/environment/useScrollPosition';
+export * from './src/hooks/environment/useTypeSafeSearchParams';
+
+// Data
+export * from './src/hooks/data/useStatefulArray';
+
+// Contexts
+export * from './src/context/CacheProvider';
